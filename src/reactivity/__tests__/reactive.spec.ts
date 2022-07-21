a35c5b9b981d9872c96e2694f555f1f617efeed8
zx-ch <549425270@qq.com>
2022-07-21 15:20:57 +0800

    init

---
+++ src/reactivity/__tests__/reactive.spec.ts
@@ -0,0 +1,9 @@
+import { reactive } from "../src/reactive";
+describe("reactive", () => {
+  it("happly path", () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+    expect(observed).not.toBe(original);
+    expect(observed.foo).toBe(1);
+  });
+});
